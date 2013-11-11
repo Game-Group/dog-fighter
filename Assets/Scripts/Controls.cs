c7f86685199604200813bb3fe18fdf26dd565211
Inge Becht <inge.becht91@gmail.com>
2013-11-11 20:37:36 +0100

    Added controls test scene again
    Hopefully it works now!

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controls : MonoBehaviour {
+	
+	bool forward;
+	// Use this for initialization
+	void Start () {
+	
+		forward = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rotate();
+		if(Input.GetKeyDown(KeyCode.W))
+		{
+			forward = true;
+		}
+		handle_input();
+	}
+	void rotate()
+	{
+		float rotationx = (Input.mousePosition.x - Screen.width/2)/80;
+		float rotationy = (Input.mousePosition.y - Screen.height/2)/80;
+		transform.Rotate(0, rotationx, 0, Space.Self);
+		transform.Rotate (-rotationy, 0, 0, Space.Self);
+	}
+	void handle_input()
+	{
+		if(forward)
+		{
+			transform.Translate(Vector3.forward);
+		}
+		
+	}
+}
---
+++ Assets/Scripts/Controls2.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controls2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float rotationx = (Input.mousePosition.x - Screen.width/2)/4;
+		float rotationy = (Input.mousePosition.y - Screen.height/2)/4;
+		transform.localRotation = Quaternion.AngleAxis (rotationx, Vector3.up) *  Quaternion.AngleAxis (rotationy + 90, Vector3.left);  
+	}
+}
