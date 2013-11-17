f9bbe4bbfa96366361f57b83b1c6a62b2258f205
Joey Deiman <joeydeiman@hotmail.com>
2013-11-17 12:21:11 +0100

    Added moved collisiondetection script.

---
+++ Assets/Scripts/Actors/DestroyAndReloadOnCollideEnter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Destroy and reload if the object hits something tagged as Obstacle.
+/// </summary>
+public class DestroyAndReloadOnCollideEnter : MonoBehaviour 
+{
+	void OnCollisionEnter(Collision info)
+	{
+		if (info.collider.tag == "Obstacle")
+		{
+			Destroy (gameObject);
+			Application.LoadLevel(Application.loadedLevelName);
+		}
+	}
+}
+
