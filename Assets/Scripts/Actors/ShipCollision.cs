5242f331c2018de8fe664953e81a365fc79270c5
Calavoow <calavoow@server.fake>
2013-11-17 13:11:20 +0100

    Started on ship collision explosion

---
+++ Assets/Scripts/Actors/ShipCollision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipCollision : MonoBehaviour {
+
+	public GameObject destroyObject;
+	public GameObject explosion;
+	public AudioClip explosionSound;
+
+	void OnCollisionEnter( Collision collision ){
+		if( collision.gameObject.tag != "PlayerProjectile" ){
+			GameObject explinst = Instantiate( explosion, destroyObject.transform.position, Quaternion.identity ) as GameObject;
+			AudioSource.PlayClipAtPoint( explosionSound, destroyObject.transform.position );
+			Destroy(destroyObject);
+			//Destroy(gameObject);
+			//Destroy( explinst, 3 );
+		}
+	}
+
+}
