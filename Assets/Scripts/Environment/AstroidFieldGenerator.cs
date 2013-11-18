0c3676c51a987616ca28cd171d57900af4184f41
tkdejong <tim@raatdejong.nl>
2013-11-18 17:21:36 +0100

    Added generated astroid ring (in Prototype0 scene).

--- Assets/Scripts/Environment/AstroidFieldGenerator.cs
+++ Assets/Scripts/Environment/AstroidFieldGenerator.cs
@@ -4,7 +4,7 @@ using System.Collections;
 /* Generates a certain amount of astroids within a bounded area.
  * The position, rotation and size of each astroid is randomized.
  */
-public class AstroidGenerator : MonoBehaviour {
+public class AstroidFieldGenerator : MonoBehaviour {
 	//Amount of astroids generated
 	public int amount = 20;
 	//Determine the max distance from the center that an astoid can be
@@ -27,14 +27,12 @@ public class AstroidGenerator : MonoBehaviour {
 
 	//Creates one randomized astroid within the bounds
 	void CreateRandomAstroid () {
-		Vector3 randomPosition = new Vector3(Random.Range(transform.position.x - maxOffset, transform.position.x + maxOffset),
-		                                     Random.Range(transform.position.y - maxOffset, transform.position.y + maxOffset),
-		                                     Random.Range(transform.position.z - maxOffset, transform.position.z + maxOffset));
+		Vector3 position = GetRandomPosition();
 		//Make sure that everything scales well
-		randomPosition.Scale(transform.localScale);
+		position.Scale(transform.localScale);
 
 		//Instantiate new astroid
-		GameObject astroid = (GameObject) GameObject.Instantiate(templateAstroid, randomPosition, Random.rotation);
+		GameObject astroid = (GameObject) GameObject.Instantiate(templateAstroid, position, Random.rotation);
 		//Scale the astroid randomly
 		astroid.transform.localScale *= Random.Range(minAstroidSize, maxAstroidSize);
 
@@ -44,10 +42,13 @@ public class AstroidGenerator : MonoBehaviour {
 		astroid.transform.name = "Astroid" + astroidCount;
 		astroidCount ++;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	//Returns a random vector3 position within the bounds
+	public virtual Vector3 GetRandomPosition() {
+		return new Vector3(Random.Range(transform.position.x - maxOffset, transform.position.x + maxOffset),
+		                                     Random.Range(transform.position.y - maxOffset, transform.position.y + maxOffset),
+		                                     Random.Range(transform.position.z - maxOffset, transform.position.z + maxOffset));
 	}
+
 }
 //""
