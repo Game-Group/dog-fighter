c0982f0367061babc390661eece2def11c1ca7e4
Inge Becht <inge.becht91@gmail.com>
2013-11-20 18:54:01 +0100

    turret now correctly looks at ship in one axis

--- Assets/Scripts/AI/turretBehaviour.cs
+++ Assets/Scripts/AI/turretBehaviour.cs
@@ -22,35 +22,18 @@ public class turretBehaviour : MonoBehaviour {
     void OnTriggerStay(Collider Object)
     {
 
-        
         // TODO check if object is spaceship
         Vector3 objPos = Object.transform.position;
         Vector3 diff = turretPos - objPos;
 
         diff.Normalize();
 
-        Vector2 u;
-        u.x = diff.x;
-        u.y = diff.z;
-        Debug.Log(u);
-        /*
-        u.Normalize();
-
-
-        Vector2 v;
-        v.x = Mathf.Cos(angle);
-        v.y = Mathf.Sin(angle);
-
-        v.Normalize();
-
-        angle = Mathf.Acos(Vector2.Dot(v, u));
-
-        Debug.Log("Angle in degrees: ");
-        top.eulerAngles = new Vector3(0, Mathf.Rad2Deg * angle, 0); 
-          */
         Vector3 lookPos = Object.transform.position - top.position;
         lookPos.y = 0;
         Quaternion rotation = Quaternion.LookRotation(lookPos);
-        top.rotation = Quaternion.Slerp(top.transform.rotation, rotation, Time.deltaTime * 5) ;
+        top.localRotation = Quaternion.Slerp(top.transform.rotation, rotation, Time.deltaTime * 5) ;
+
+
+
     }
 }
