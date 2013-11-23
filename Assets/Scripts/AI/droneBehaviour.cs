3b9de6bf3b7f91ba418036b81983a5998f34becf
Inge Becht <inge.becht91@gmail.com>
2013-11-23 21:29:14 +0100

    improved drone behaviour slightly
    by now shooting rays at a 40 degree angles from the center of the  drone
    instead of straight shooting

--- Assets/Scripts/AI/droneBehaviour.cs
+++ Assets/Scripts/AI/droneBehaviour.cs
@@ -15,7 +15,8 @@ public Transform target;
         
         Vector3 direction = recalculatePath();
         // Mooove
-        var rot = Quaternion.LookRotation(direction);
+        Quaternion rot = Quaternion.LookRotation(direction);
+
         transform.rotation = Quaternion.Slerp(transform.rotation, rot, Time.deltaTime);
 
         transform.position += transform.forward * 5f * Time.deltaTime;
@@ -33,12 +34,16 @@ public Transform target;
         Vector3 direction = transform.forward;
 
         RaycastHit hitInfo;
-        float distance = 20;
+        float distance = 10;
       
-        Vector3 leftR = origin;
-        Vector3 rightR = origin;
-        leftR.x -= 2;
-        rightR.x += 2;
+        GameObject temp = new GameObject();
+        Vector3 t = origin;
+       
+        temp.transform.position = t;
+        Debug.Log(transform.localEulerAngles);
+        temp.transform.localEulerAngles = transform.localEulerAngles;
+
+       
 
         // Shoot 3 different rays
         // One from the center of the drone to the fron
@@ -48,27 +53,37 @@ public Transform target;
         {
             if (hitInfo.transform != transform)
             {
+
+                Debug.DrawLine(origin, hitInfo.point, Color.red);
+                Debug.DrawLine(hitInfo.point, hitInfo.normal, Color.blue);
                 dir += hitInfo.normal * 20;
             }
         }
 
-        if (Physics.Raycast(leftR, direction, out hitInfo, distance))
+        temp.transform.localEulerAngles = new Vector3(0, 40 + transform.localEulerAngles.y, 0);
+        if (Physics.Raycast(origin, temp.transform.forward, out hitInfo, distance))
         {
             if (hitInfo.transform != transform)
             {
+
+                Debug.DrawLine(origin, hitInfo.point, Color.red);
+                Debug.DrawLine(hitInfo.point, hitInfo.normal, Color.blue);
                 dir += hitInfo.normal * 20;
             }
         }
 
-        if (Physics.Raycast(rightR, direction, out hitInfo, distance))
+        temp.transform.localEulerAngles = new Vector3(0, -40 + transform.localEulerAngles.y, 0);
+        if (Physics.Raycast(origin, temp.transform.forward, out hitInfo, distance))
         {
 
             if (hitInfo.transform != transform)
             {
+
+                Debug.DrawLine(origin, hitInfo.point, Color.red);
+                Debug.DrawLine(hitInfo.point, hitInfo.normal, Color.blue);
                 dir += hitInfo.normal * 20;
             }
         }
-
         return dir;
     }
 
